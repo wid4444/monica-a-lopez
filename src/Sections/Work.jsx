b05cba23154f82a144560ec61d0850e08ce7cc05
Leobardo D. Martinez-Quiroz <ldmartinezquiroz@email.com>
2025-07-11 23:22:34 -0400

    tried to modified the images in the portfolio section to go directly yo the gallery if clicked

--- src/Sections/Work.jsx
+++ src/Sections/Work.jsx
@@ -1,30 +1,34 @@
-import './Work.css'
-import Modal from './Modal'
+import "./Work.css";
+import Modal from "./Modal";
 
 import acrylics4 from "../assets/acrylics/4.jpg";
 import wood1 from "../assets/wood/1.jpg";
-import video1 from "../assets/aluminio/video1.mp4";
-
+// import video1 from "../assets/aluminio/video1.mp4";
 
 const Work = () => {
-  return (
-    <div id='work' className='work-container'>
-<div className="pic-container">
-  <img src={acrylics4} alt="" className='pic neon'/>
-  <img src={wood1} alt="" className='pic pic-2'/>
-  <img src={acrylics4} alt="" className='pic pic-3'/>
-  <img src={acrylics4} alt="" className='pic pic-4'/>
-  <img src={acrylics4} alt="" className='pic pic-5'/>
-  <img src={acrylics4} alt="" className='pic pic-6'/>
-  <img src={acrylics4} alt="" className='pic pic-7'/>
-  <img src={acrylics4} alt="" className='pic pic-8'/>
-</div>
-<div className="modal-container">
-        <Modal />
-        </div>
-    
-    </div>
-  )
-}
+  const images = [
+    acrylics4,
+    wood1,
+    acrylics4,
+    acrylics4,
+    acrylics4,
+    acrylics4,
+    acrylics4,
+    acrylics4,
+  ];
 
-export default Work
+  return (
+    <div id="work" className="work-container">
+      <div className="pic-container">
+        {images.map((src, i) => (
+          <img key={i} src={src} alt="" className="pic" />
+        ))}
+      </div>
+      <div className="modal-container">
+        <Modal />
+      </div>
+    </div>
+  );
+};
+
+export default Work;
